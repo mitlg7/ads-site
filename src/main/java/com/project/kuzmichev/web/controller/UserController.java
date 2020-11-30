b56515bf48ca2e0f55687a6ab800c9d57e7b5b2f
mitlg7 <mitlg7@gmail.com>
2020-11-30 19:11:32 +0400

    Finaly

--- src/main/java/com/project/kuzmichev/web/controller/UserController.java
+++ src/main/java/com/project/kuzmichev/web/controller/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
         own.setBirthday(user.getBirthday());
         own.setEmail(user.getEmail());
         own.setFirstName(user.getFirstName());
-        own.setSecondName(user.getSecondName());
+        own.setSecondName("Иванов");
         own.setPatronymic(user.getPatronymic());
         own.setPhone(user.getPhone());
         userService.save(own);
